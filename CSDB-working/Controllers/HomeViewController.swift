f7681451f68f9894e5963feb7568b6b060111f40
jack battle <jackonno78@hotmail.com>
2021-10-08 15:01:10 -0500

    working stat retriever and displays stats, need to still update profile picture

--- CSDB-working/Controllers/HomeViewController.swift
+++ CSDB-working/Controllers/HomeViewController.swift
@@ -12,26 +12,69 @@ import UIKit
 import Alamofire
 
 class HomeViewController: UIViewController {
+    
     @IBOutlet weak var searchField: UITextField!
     
-    let playerRequestManager = PlayerRequestManager()
+    var playerRequestManager = PlayerRequestManager()
+    var retrievedPlayer: PlayerModel! {
+        didSet{
+            self.performSegue(withIdentifier: "searchToStats", sender: self)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
- 
+    
     @IBAction func searchButtonPressed(_ sender: Any) {
         if searchField.text != nil {
             let searchedName = searchField.text!
-            playerRequestManager.fetchPlayerByName("\(searchedName)")
+            playerRequestManager.fetchPlayerByName("\(searchedName)") { [weak self] playerObject in
+                print(playerObject.playerMoneyEarned)
+                self?.retrievedPlayer = playerObject
+//                self?.performSegue(withIdentifier: "searchToStats", sender: self)
+            }
         }else if searchField.text == nil {
             print("no name entered.")
         }else {
             print("unknown error")
         }
-//        playerRequestManager.fetchPlayerByName("jackonno78")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "searchToStats" {
+            let destinationVC = segue.destination as! PlayerStatsViewController
+            
+            destinationVC.username = retrievedPlayer!.playerUsername
+            destinationVC.avatarURL = retrievedPlayer!.avatarURL
+            destinationVC.timePlayed = retrievedPlayer!.playerTimePlayed
+            
+            destinationVC.kills = retrievedPlayer!.playerKills
+            destinationVC.deaths = retrievedPlayer!.playerDeaths
+            destinationVC.kd = retrievedPlayer!.playerKD
+            destinationVC.damage = retrievedPlayer!.playerDamage
+            
+            destinationVC.matchesPlayed = retrievedPlayer!.playerMaychesPlayed
+            destinationVC.wins = retrievedPlayer!.playerWins
+            destinationVC.losses = retrievedPlayer!.playerLosses
+            destinationVC.ties = retrievedPlayer!.playerTies
+            destinationVC.mvps = retrievedPlayer!.playerMVPs
+            destinationVC.moneyEarned = retrievedPlayer!.playerMoneyEarned
+            
+            destinationVC.headshotKills = retrievedPlayer!.playerHeadshots
+            destinationVC.headshotPct = retrievedPlayer!.playerHeadshotPct
+            destinationVC.shotsHit = retrievedPlayer!.playerShotsHit
+            destinationVC.shotsFired = retrievedPlayer!.playerShotsFired
+            destinationVC.shotAccuracy = retrievedPlayer!.playerShotsAccuracy
+            
+            destinationVC.bombsPlanted = retrievedPlayer!.playerBombsPlanted
+            destinationVC.bombsDefused = retrievedPlayer!.playerBombsDefused
+            destinationVC.hostagesRescued = retrievedPlayer!.playerHostagesRescued
+            
+            destinationVC.roundsPlayed = retrievedPlayer!.playerRoundsPlayed
+            destinationVC.wlPct = retrievedPlayer!.playerWLPercentage
+        }
     }
 }
-
 
--- CSDB-working/Controllers/PlayerStatsViewController.swift
+++ CSDB-working/Controllers/PlayerStatsViewController.swift
@@ -11,37 +11,89 @@ import UIKit
 class PlayerStatsViewController: UIViewController {
     //MARK: - user info labels
     @IBOutlet weak var profilePictureView: UIImageView!
+    var avatarURL: String = ""
     @IBOutlet weak var usernameLabel: UILabel!
+    var username: String = ""
     //MARK: - time played label
     @IBOutlet weak var timePlayedLabel: UILabel!
+    var timePlayed: String = ""
     //MARK: - match info labels
     @IBOutlet weak var matchesPlayedLabel: UILabel!
+    var matchesPlayed: String = ""
     @IBOutlet weak var winsLabel: UILabel!
+    var wins: String = ""
     @IBOutlet weak var lossesLabel: UILabel!
+    var losses: String = ""
     @IBOutlet weak var tiesLabel: UILabel!
+    var ties: String = ""
     @IBOutlet weak var mvpLabel: UILabel!
+    var mvps: String = ""
     @IBOutlet weak var moneyEarnedLabel: UILabel!
+    var moneyEarned: String = ""
     //MARK: - objective labels
     @IBOutlet weak var bombsPlantedLabel: UILabel!
+    var bombsPlanted: String = ""
     @IBOutlet weak var bombsDefusedLabel: UILabel!
+    var bombsDefused: String = ""
     @IBOutlet weak var hostagesRescuedLabel: UILabel!
+    var hostagesRescued: String = ""
     //MARK: - kills labels
     @IBOutlet weak var killsLabel: UILabel!
+    var kills: String = ""
     @IBOutlet weak var deathsLabel: UILabel!
+    var deaths: String = ""
     @IBOutlet weak var kdLabel: UILabel!
+    var kd: String = ""
     @IBOutlet weak var damageLabel: UILabel!
+    var damage: String = ""
     //MARK: - accuracy labels
     @IBOutlet weak var headshotKillsLabel: UILabel!
+    var headshotKills: String = ""
     @IBOutlet weak var headshotPctLabel: UILabel!
+    var headshotPct: String = ""
     @IBOutlet weak var shotsHitLabel: UILabel!
+    var shotsHit: String = ""
     @IBOutlet weak var shotsFiredLabel: UILabel!
+    var shotsFired: String = ""
     @IBOutlet weak var shotAccuracyLabel: UILabel!
+    var shotAccuracy: String = ""
     //MARK: - rounds labels
     @IBOutlet weak var roundsPlayedLabel: UILabel!
+    var roundsPlayed: String = ""
     @IBOutlet weak var wlPctLabel: UILabel!
+    var wlPct: String = ""
+    //MARK: - actual view controller stuff
+    
+    
+    
     
     override func viewDidLoad() {
         print("successfully loaded second VC")
+        
+        usernameLabel?.text = username
+        
+        timePlayedLabel.text = timePlayed
+        
+        killsLabel.text = kills
+        deathsLabel.text = deaths
+        kdLabel.text = kd
+        damageLabel.text = damage
+        
+        matchesPlayedLabel.text = matchesPlayed
+        winsLabel.text = wins
+        lossesLabel.text = losses
+        tiesLabel.text = ties
+        mvpLabel.text = mvps
+        moneyEarnedLabel.text = moneyEarned
+        
+        headshotKillsLabel.text = headshotKills
+        headshotPctLabel.text = headshotPct
+        shotsHitLabel.text = shotsHit
+        shotsFiredLabel.text = shotsFired
+        shotAccuracyLabel.text = shotAccuracy
+        
+        roundsPlayedLabel.text = roundsPlayed
+        wlPctLabel.text = wlPct
     }
     
 }
